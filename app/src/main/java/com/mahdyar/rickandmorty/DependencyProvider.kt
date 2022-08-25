facf64d72511afa3f0d223d6aa59a1b364a406c0
Ani <anahidmansoubi2015@gmail.com>
2022-08-25 17:26:19 -0400

    Add service to DependencyProvider

--- app/src/main/java/com/mahdyar/rickandmorty/DependencyProvider.kt
+++ app/src/main/java/com/mahdyar/rickandmorty/DependencyProvider.kt
@@ -1,5 +1,6 @@
 package com.mahdyar.rickandmorty
 
+import com.mahdyar.rickandmorty.data.ApiService
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
@@ -7,19 +8,23 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 
 class DependencyProvider {
 
-    fun provideBaseUrl(): String {
+    private fun provideBaseUrl(): String {
         return BASE_URL
     }
 
-    fun provideMoshi(): Moshi {
+    private fun provideMoshi(): Moshi {
         return Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
     }
 
-    fun provideRetrofit(): Retrofit {
+    private fun provideRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl(provideBaseUrl())
             .addConverterFactory(MoshiConverterFactory.create(provideMoshi()))
             .build()
+    }
+
+    fun provideService(): ApiService {
+        return provideRetrofit().create(ApiService::class.java)
     }
 
     companion object {
