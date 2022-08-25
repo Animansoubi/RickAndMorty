a3324fb3878f377f0395fd66288a025fbfd4a707
Ani <anahidmansoubi2015@gmail.com>
2022-08-25 17:13:32 -0400

    Add retrofit to dependencyProvider

--- app/src/main/java/com/mahdyar/rickandmorty/DependencyProvider.kt
+++ app/src/main/java/com/mahdyar/rickandmorty/DependencyProvider.kt
@@ -2,6 +2,8 @@ package com.mahdyar.rickandmorty
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class DependencyProvider {
 
@@ -11,6 +13,13 @@ class DependencyProvider {
 
     fun provideMoshi(): Moshi {
         return Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+    }
+
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(provideBaseUrl())
+            .addConverterFactory(MoshiConverterFactory.create(provideMoshi()))
+            .build()
     }
 
     companion object {
--- app/src/main/java/com/mahdyar/rickandmorty/network/ApiClient.kt
+++ app/src/main/java/com/mahdyar/rickandmorty/network/ApiClient.kt
@@ -2,18 +2,12 @@ package com.mahdyar.rickandmorty.network
 
 import com.mahdyar.rickandmorty.DependencyProvider
 import com.mahdyar.rickandmorty.data.ApiService
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 
 object ApiClient {
     private val dependencyProvider = DependencyProvider()
 
-    private val retrofit: Retrofit by lazy {
-        Retrofit.Builder()
-            .baseUrl(dependencyProvider.provideBaseUrl())
-            .addConverterFactory(MoshiConverterFactory.create(dependencyProvider.provideMoshi()))
-            .build()
-    }
+    private val retrofit = dependencyProvider.provideRetrofit()
+
     val apiService: ApiService by lazy {
         retrofit.create(ApiService::class.java)
     }
